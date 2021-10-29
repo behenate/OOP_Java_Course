2c955243e64b09625521ef759f93521e2674b6d6
behenate <behenate@gmail.com>
2021-10-29 22:21:39 +0200

    Nowa wersja testów integracyjnych

--- src/test/java/agh/ics/oop/AnimalTest.java
+++ src/test/java/agh/ics/oop/AnimalTest.java
@@ -3,93 +3,83 @@ package agh.ics.oop;
 import agh.ics.oop.*;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AnimalTest {
-    Animal testAnimal;
-
+    private final Animal testAnimal = new Animal();
+    private final OptionsParser testParser = new OptionsParser();
     @Test
-    void testOrientation() {
-        testAnimal = new Animal();
-        assertEquals(MapDirection.NORTH, testAnimal.getDirection());
-        testAnimal.move(MoveDirection.FORWARD);
-        assertEquals(MapDirection.NORTH, testAnimal.getDirection());
-        testAnimal.move(MoveDirection.BACKWARD);
-        assertEquals(MapDirection.NORTH, testAnimal.getDirection());
+    void integrationTests(){
 
-        testAnimal.move(MoveDirection.LEFT);
-        assertEquals(MapDirection.WEST, testAnimal.getDirection());
-        testAnimal.move(MoveDirection.LEFT);
-        assertEquals(MapDirection.SOUTH, testAnimal.getDirection());
-        testAnimal.move(MoveDirection.LEFT);
-        assertEquals(MapDirection.EAST, testAnimal.getDirection());
-        testAnimal.move(MoveDirection.LEFT);
-        assertEquals(MapDirection.NORTH, testAnimal.getDirection());
+//        Test poprawnej orientacji
+        String[] input1 = {"r", "r","r","r", "l","l","l","l"};
+        ArrayList<MoveDirection> pInput1 = testParser.parse(input1);
+        MapDirection[] output1 = {MapDirection.NORTH, MapDirection.EAST, MapDirection.SOUTH, MapDirection.WEST, MapDirection.NORTH, MapDirection.WEST, MapDirection.SOUTH, MapDirection.EAST, MapDirection.NORTH};
+        for (int i = 0; i < pInput1.size(); i++) {
+            assertEquals(output1[i], testAnimal.getDirection());
+            testAnimal.move(pInput1.get(i));
+        }
+//        Test przemieszczenia i wyjścia poza mapę dla kolejno North East South West:
+        boundsMoveTest(0, 0, 1);
+        boundsMoveTest(1, 1, 0);
+        boundsMoveTest(2, 0, -1);
+        boundsMoveTest(3, -1, 0);
 
-        testAnimal.move(MoveDirection.RIGHT);
-        assertEquals(MapDirection.EAST, testAnimal.getDirection());
-        testAnimal.move(MoveDirection.RIGHT);
-        assertEquals(MapDirection.SOUTH, testAnimal.getDirection());
-        testAnimal.move(MoveDirection.RIGHT);
-        assertEquals(MapDirection.WEST, testAnimal.getDirection());
-        testAnimal.move(MoveDirection.RIGHT);
-        assertEquals(MapDirection.NORTH, testAnimal.getDirection());
+//        Test parsera
+//        Dane poniżej bez śmieci: ,"f", "b","r", "l", "forward", "backward","left","right"
+        String[] input2 = {"trololo","f","blabla", "b","rr", "r","ll f b r l", "l", "forward","kupol", "backward","forwardfas","left","smiec","right"};
+        ArrayList<MoveDirection> pInput2 = testParser.parse(input2);
+        MapDirection[] outputMapDir = {MapDirection.NORTH, MapDirection.NORTH, MapDirection.NORTH, MapDirection.EAST,
+                MapDirection.NORTH, MapDirection.NORTH, MapDirection.NORTH, MapDirection.WEST, MapDirection.NORTH};
+        int[][] outPosInt = {{2,2}, {2,3}, {2,2}, {2,2}, {2,2}, {2,3},{2,2},{2,2},{2,2}};
+        Vector2d[] outputPos = new Vector2d[outPosInt.length];
+        for (int i = 0; i < outPosInt.length; i++) {
+            outputPos[i] = new Vector2d(outPosInt[i][0], outPosInt[i][1]);
+        }
+        for (int i = 0; i < pInput2.size(); i++) {
+            assertEquals(outputMapDir[i], testAnimal.getDirection());
+            assertEquals(outputPos[i], testAnimal.getPosition());
+            testAnimal.move(pInput2.get(i));
+        }
     }
 
     /*
-        Funkcja testująca dla moveTest. Obraca zwierzaka podaną liczbę razy w prawo, następnie próbuje nim wyjść
-        poza mapę przodem i tyłem. Dla każdego ruchu sprawdzane jest czy pozycja jest poprawna.
+        Funkcja testująca. Obraca zwierzaka podaną liczbę razy w prawo, następnie próbuje nim wyjść
+        poza mapę przodem i tyłem. Dla każdego ruchu sprawdzane jest czy pozycja jest poprawna. x_vec i y_vec to
+        skłądowe x i y wektora ruchu któru powinien wykonać zwierzak
     */
     void boundsMoveTest(int rotations, int x_vec, int y_vec) {
         Animal testAnimal = new Animal();
         int expected_x = 2;
         int expected_y = 2;
+        String[] input = new String[rotations+12];
         for (int i = 0; i < rotations; i++) {
-            testAnimal.move(MoveDirection.RIGHT);
+            input[i] = "r";
         }
         for (int i = 0; i < 6; i++) {
-            testAnimal.move(MoveDirection.FORWARD);
-            expected_x = Math.min(Math.max(expected_x + x_vec, 0), 4);
-            expected_y = Math.min(Math.max(expected_y + y_vec, 0), 4);
-            assertEquals(new Vector2d(expected_x, expected_y), testAnimal.getPosition());
-
+            input[i+rotations] = "f";
         }
         for (int i = 0; i < 6; i++) {
-            testAnimal.move(MoveDirection.BACKWARD);
-            expected_x = Math.min(Math.max(expected_x - x_vec, 0), 4);
-            expected_y = Math.min(Math.max(expected_y - y_vec, 0), 4);
-            assertEquals(new Vector2d(expected_x, expected_y), testAnimal.getPosition());
+            input[i+6+rotations] = "b";
         }
-    }
-
-    @Test
-    void testMove() {
-//      Dla north
-        boundsMoveTest(0, 0, 1);
-//      Dla East
-        boundsMoveTest(1, 1, 0);
-//      Dla South
-        boundsMoveTest(2, 0, -1);
-//      Dla West
-        boundsMoveTest(3, -1, 0);
-    }
-
-    @Test
-    void testParser() {
-        OptionsParser parser = new OptionsParser();
-        String[] testInput1 = {"f", "v", "a", "b", "r", "l", "lalalala", "trolololo", "forward"};
-        MoveDirection[] expectedOutput1 = {MoveDirection.FORWARD, MoveDirection.BACKWARD, MoveDirection.RIGHT,
-                MoveDirection.LEFT, MoveDirection.FORWARD};
-        assertArrayEquals(expectedOutput1, parser.parse(testInput1).toArray());
-        String[] testInput2 = {"f", "b", "l", "r", "forward", "backward", "left", "right"};
-        MoveDirection[] expectedOutput2 = {MoveDirection.FORWARD, MoveDirection.BACKWARD, MoveDirection.LEFT,
-                MoveDirection.RIGHT, MoveDirection.FORWARD, MoveDirection.BACKWARD, MoveDirection.LEFT, MoveDirection.RIGHT};
-        assertArrayEquals(expectedOutput2, parser.parse(testInput2).toArray());
-        String[] testInput3 = {"f","sia", "b","la", "l","la", "r","ba", "forward","fa", "backward","trololo",
-                "left","kupol", "right"};
-        MoveDirection[] expectedOutput3 = {MoveDirection.FORWARD, MoveDirection.BACKWARD, MoveDirection.LEFT,
-                MoveDirection.RIGHT, MoveDirection.FORWARD, MoveDirection.BACKWARD, MoveDirection.LEFT, MoveDirection.RIGHT};
-        assertArrayEquals(expectedOutput3, parser.parse(testInput3).toArray());
+//       // Zparsowanie wygenerowanej sekwencji ruchów
+        ArrayList<MoveDirection> pInput = testParser.parse(input);
+        for (int i = 0; i < rotations; i++) {
+            testAnimal.move(pInput.get(i));
+        }
+        for (int j = 0; j < 2; j++) {
+//          //Sign zmienia znak dodawania wektora - w drugiej iteracji wykonywane jest 6 ruchów w tył.
+            int sign = 1 - j*2;
+            for (int i = 0; i < 6; i++) {
+                testAnimal.move(pInput.get(rotations+i+(j*6)));
+                expected_x = Math.min(Math.max(expected_x + (x_vec*sign), 0), 4);
+                expected_y = Math.min(Math.max(expected_y + (y_vec*sign), 0), 4);
+                assertEquals(new Vector2d(expected_x, expected_y), testAnimal.getPosition());
+            }
+        }
     }
 }
